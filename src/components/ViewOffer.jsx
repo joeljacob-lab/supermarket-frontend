4d17a754e162de7253e52672877e65a28b673342
AnaghaAjidev <anaghaajidev@gmail.com>
2026-07-15 13:02:45 +0530

    Completed

--- src/components/ViewOffer.jsx
+++ src/components/ViewOffer.jsx
@@ -1,81 +1,92 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const ViewOffer = () => {
 
-    const [data, changeData] = useState([])
+    const [data, changeData] = useState([]);
 
     const fetchData = () => {
-        axios.get("http://localhost:3000/view_offer")
+
+        axios.post("http://localhost:3000/view_offer", {})
             .then((response) => {
-                changeData(response.data)
+                changeData(response.data);
             })
             .catch((error) => {
-                console.log(error)
-            })
-    }
+                console.log(error);
+            });
+
+    };
 
     useEffect(() => {
-        fetchData()
-    }, [])
+        fetchData();
+    }, []);
 
     return (
-        <div>
+        <div
+            className="container-fluid"
+            style={{ backgroundColor: "#d6d6d6", minHeight: "100vh" }}
+        >
+            <div className="row">
+                <div className="col col-sm-12">
 
+                    <h2 className="text-center mt-4 mb-4">
+                        <b>VIEW OFFERS</b>
+                    </h2>
 
+                    <div className="table-responsive">
 
-            <div className="container mt-4">
+                        <table className="table table-bordered table-striped table-hover">
 
-                <h2 className="text-center mb-4">
-                    <b>VIEW OFFERS</b>
-                </h2>
-
-                <div className="table-responsive">
-                    <table className="table table-bordered table-hover table-striped">
-                        <thead className="table-dark">
-                            <tr>
-                                <th>Offer ID</th>
-                                <th>Offer Name</th>
-                                <th>Offer Type</th>
-                                <th>Product Category</th>
-                                <th>Discount %</th>
-                                <th>Minimum Purchase</th>
-                                <th>Start Date</th>
-                                <th>End Date</th>
-                                <th>Applicable Products</th>
-                                <th>Coupon Code</th>
-                                <th>Status</th>
-                                <th>Offer Description</th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            {data.map((value, index) => (
-                                <tr key={index}>
-                                    <td>{value.OfferID}</td>
-                                    <td>{value.OfferName}</td>
-                                    <td>{value.OfferType}</td>
-                                    <td>{value.ProductCategory}</td>
-                                    <td>{value.DiscountPercentage}</td>
-                                    <td>{value.MinimumPurchaseAmount}</td>
-                                    <td>{value.StartDate}</td>
-                                    <td>{value.EndDate}</td>
-                                    <td>{value.ApplicableProducts}</td>
-                                    <td>{value.CouponCode}</td>
-                                    <td>{value.Status}</td>
-                                    <td>{value.OfferDescription}</td>
+                            <thead className="table-dark">
+                                <tr>
+                                    <th>Offer ID</th>
+                                    <th>Offer Name</th>
+                                    <th>Offer Type</th>
+                                    <th>Product Category</th>
+                                    <th>Discount %</th>
+                                    <th>Minimum Purchase</th>
+                                    <th>Start Date</th>
+                                    <th>End Date</th>
+                                    <th>Applicable Products</th>
+                                    <th>Coupon Code</th>
+                                    <th>Status</th>
+                                    <th>Description</th>
                                 </tr>
-                            ))}
-                        </tbody>
+                            </thead>
 
-                    </table>
+                            <tbody>
+
+                                {
+                                    data.map((value, index) => {
+                                        return (
+                                            <tr key={index}>
+                                                <td>{value.offer_id}</td>
+                                                <td>{value.offer_name}</td>
+                                                <td>{value.offer_type}</td>
+                                                <td>{value.product_category}</td>
+                                                <td>{value.discount_percentage}</td>
+                                                <td>{value.minimum_purchase_amount}</td>
+                                                <td>{value.start_date}</td>
+                                                <td>{value.end_date}</td>
+                                                <td>{value.applicable_products}</td>
+                                                <td>{value.coupon_code}</td>
+                                                <td>{value.status}</td>
+                                                <td>{value.offer_description}</td>
+                                            </tr>
+                                        );
+                                    })
+                                }
+
+                            </tbody>
+
+                        </table>
+
+                    </div>
+
                 </div>
-
             </div>
-
         </div>
-    )
-}
+    );
+};
 
-export default ViewOffer
+export default ViewOffer;
